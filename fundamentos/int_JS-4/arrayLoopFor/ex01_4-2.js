525449997d01e39aff2ecfae36f499558d389ef8
SherlyAmorim <sherlyamorim@gmail.com>
2022-12-02 19:22:39 -0300

    Exercício 1 de Array e Loop For

---
+++ fundamentos/int_JS-4/arrayLoopFor/ex01_4-2.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+console.log(numbers);
+console.log('');
+
+for (let index = 0; index < numbers.length; index += 1) {
+	console.log(numbers[index]);
+}
