1333de3d0f8896c56d8f2ee0243b448dfbe07a91
SherlyAmorim <sherlyamorim@gmail.com>
2022-12-03 17:43:05 -0300

    Corrigindo exercício 09_4.2

--- fundamentos/int_JS-4/arrayLoopFor/ex09_4-2.js
+++ fundamentos/int_JS-4/arrayLoopFor/ex09_4-2.js
@@ -1,6 +1,5 @@
 // Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
 
-
 let numbers = [];
 
 for (let index = 1; index <= 25; index += 1){
@@ -8,3 +7,15 @@ for (let index = 1; index <= 25; index += 1){
 }
 console.log(numbers);
 
+for (let index = 0; index < numbers.length; index += 1){
+        console.log(numbers[index]/2);
+}
+
+console.log('');
+console.log('----- Usando For/Of -----');
+console.log('');
+
+for (let values of numbers){
+    let result = values / 2;
+    console.log(result);
+}
