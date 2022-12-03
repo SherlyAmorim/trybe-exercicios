742fe223a8e1d24e8e952eb340746e5192a8668c
SherlyAmorim <sherlyamorim@gmail.com>
2022-12-03 14:50:47 -0300

    Realizado exercício 07_4.2

---
+++ fundamentos/int_JS-4/arrayLoopFor/ex07_4-2.js
@@ -0,0 +1,12 @@
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let smaller = numbers[0];
+
+for (let index = 0; index < numbers.length; index += 1){
+    if (smaller > numbers[index]){
+        smaller = numbers[index];
+    }
+}
+console.log(smaller);
+
