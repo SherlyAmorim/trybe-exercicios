67c2f4593bbdb38dc054daeb292494d5b3303b3b
SherlyAmorim <sherlyamorim@gmail.com>
2022-12-02 23:56:03 -0300

    Melhorando o exercício 01_4.2

--- fundamentos/int_JS-4/arrayLoopFor/ex01_4-2.js
+++ fundamentos/int_JS-4/arrayLoopFor/ex01_4-2.js
@@ -3,3 +3,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index += 1) {
 	console.log(numbers[index]);
 }
+
+console.log('-------------------------');
+console.log('----- Usando For/Of -----');
+console.log('-------------------------');
+
+
+for (let valores of numbers){
+    console.log(valores);
+}
