078798f96946c971cc9d58b463e48ce5c93d3ee9
SherlyAmorim <sherlyamorim@gmail.com>
2022-12-03 00:35:57 -0300

    Colocando os títulos nos exercícios 01 à 05 _4.2

--- fundamentos/int_JS-4/arrayLoopFor/ex02_4-2.js
+++ fundamentos/int_JS-4/arrayLoopFor/ex02_4-2.js
@@ -1,3 +1,5 @@
+// Some todos os valores contidos no array e imprima o resultado;
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let result = 0;
 
--- fundamentos/int_JS-4/arrayLoopFor/ex03_4-2.js
+++ fundamentos/int_JS-4/arrayLoopFor/ex03_4-2.js
@@ -1,3 +1,7 @@
+// Calcule e imprima a média aritmética dos valores contidos no array;
+// A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 
