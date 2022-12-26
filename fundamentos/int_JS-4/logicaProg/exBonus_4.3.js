633defd0fac48bb0c0edaa7e22b4ab9389e3ad72
SherlyAmorim <sherlyamorim@gmail.com>
2022-12-25 23:31:13 -0300

    Resolvendo mais exercícios

--- fundamentos/int_JS-4/logicaProg/exBonus_4.3.js
+++ fundamentos/int_JS-4/logicaProg/exBonus_4.3.js
@@ -1,15 +1,35 @@
 // Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n:
 
 let n = 5;
-let repeat = [];
+let simbolo = '*';
+let repeat = '';
 
-for (let index = 1; index <= n; index += 1){
-    console.log(repeat += ['*']);
+for (let index = 0; index < n; index += 1){
+    repeat = repeat + simbolo;
 }
-
+for (let index2 = 0; index2 < n; index2 += 1){ 
+    console.log(repeat);
+}
+console.log('');
+console.log('');
 
 // Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
 
-// for (let index2 = 0; index2 < n; index2 += 1){ 
-//     console.log(repeat += ['*']);
+let n1 = 5;
+let repeat1 = [];
+
+for (let index = 0; index < n1; index += 1){
+    console.log(repeat1 += ['*']);
+}
+
+console.log('');
+console.log('');
+
+// Agora inverta o lado do triângulo. Por exemplo:
+
+// let n2 = 5;
+// let repeat2 = [];
+
+// for (let index = 0; index < n2; index += 1){
+//     console.log(repeat2 += ['*']);
 // }
