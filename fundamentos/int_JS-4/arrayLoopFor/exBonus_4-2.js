9107a92640b3b483f05f9db831357ea0149b1afa
SherlyAmorim <sherlyamorim@gmail.com>
2022-12-26 17:27:14 -0300

    Concluíndo o exercício bônus

--- fundamentos/int_JS-4/arrayLoopFor/exBonus_4-2.js
+++ fundamentos/int_JS-4/arrayLoopFor/exBonus_4-2.js
@@ -1,4 +1,4 @@
-// Ordene o array numbers em ordem crescente e imprima seus valores;
+// // Ordene o array numbers em ordem crescente e imprima seus valores;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
@@ -34,11 +34,16 @@ console.log(numbers);
 
 // Agora, crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
 
-// let values = [];
+let values = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// for (let index = 1; index < numbers.length; index += 1){
-//     for (let index2 = 0; index2 < index; index2 += 1){
-//         values = index * index2;
-//         multiply.push(values);
-//     }
-// }
+let multiply = [];
+
+for (let index = 0; index < values.length; index += 1){
+    if(index + 1 < values.length){        
+        multiply.push(values[index] * values[index + 1]);        
+    }
+    else {        
+        multiply.push(values[index] * 2);        
+    }
+}
+console.log(multiply);
